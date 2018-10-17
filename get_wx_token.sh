48564844a6536f6b71b4606653888311e05bc8e7
d9394 <9394952@gmail.com>
2018-10-17 09:13:41 +0800

    Update get_wx_token.sh
    sometimes, when the proxy is not working or something wrong, the old script isn't work and get a blank token

--- get_wx_token.sh
+++ get_wx_token.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 ###SCRIPT_NAME:get_wx_token.sh###
 ###get weixin token###
-###V1-2018-09-06###
+###V2-2018-10-17###
+
+AA=$(ps aux | grep -v grep | grep "get_wx_token" | wc -l)
+if [ $AA -gt 2 ]; then
+  echo get wx token process $AA
+  exit 1
+fi
+
+Get_Token(){
+  Result=""
+  while true
+  do
+    Result=$(/usr/bin/curl --proxy http://192.168.1.1:8080 -s -G $1 | sed "s/,/\n/g" | grep token | cut -d ":" -f 2 | sed "s/\"//g")
+    if [ "$Result" != "" ]; then
+      echo $Result
+      break
+    fi
+    sleep 5
+  done
+}
 
 CropID='wx123456789'
 Secret='123456789123456789123456789123456789'
 GURL="https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=$CropID&corpsecret=$Secret"
 #if using proxy 192.168.1.1:8080
-Gtoken=$(/usr/bin/curl --proxy http://192.168.1.1:8080 -s -G $GURL | sed "s/,/\n/g" | grep token | cut -d ":" -f 2 | sed "s/\"//g")
+Gtoken=$(Get_Token $GURL)
 echo $Gtoken > /tmp/wx_token.txt
